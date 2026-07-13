7c3cf43c6bd4620bbc1f88d1c42283c90807b81b
Koln Buenito <kolnbuenito@Cologne-Buenito-MacBook-Air.local>
2026-07-13 17:13:18 +0800

    Fix syntax error in build script

--- build.sh
+++ build.sh
@@ -1,26 +1,16 @@
 #!/bin/sh
 
-# 1. Install PHP & tools needed for Statamic
+# 1. Update and install PHP + tools needed for Statamic
 dnf clean metadata
-dnf install -y php8.2 php8.2-{common,mbstring,gd,bcmath,xml,fpm,intl,zip} dnf install -y wget
+dnf install -y php8.2 php8.2-common php8.2-mbstring php8.2-gd php8.2-bcmath php8.2-xml php8.2-fpm php8.2-intl php8.2-zip wget
 
 # 2. Download and install Composer
-EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
-fi
-
+wget -O composer-setup.php https://getcomposer.org/installer
 php composer-setup.php --quiet
 rm composer-setup.php
 
-# 3. Install your project's PHP code packages
-php composer.phar install
+# 3. Install your project's PHP packages
+php composer.phar install --no-dev --prefer-dist --optimize-autoloader
 
 # 4. Generate the secure environment key
 php artisan key:generate
